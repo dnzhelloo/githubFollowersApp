4873c0b1daffa9f64e9f2c305a4e1428714ea33d
dnzhelloo <dnzhelloo@icloud.com>
2023-03-26 18:23:47 +0300

    custom views UIButton, UITextField

---
+++ githubFollowersApp/GFTextField.swift
@@ -0,0 +1,40 @@
+//
+//  GFTextField.swift
+//  githubFollowersApp
+//
+//  Created by deniz on 26.03.2023.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure(){
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        textAlignment = .center
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter username"
+    }
+}
