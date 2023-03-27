8b90c926d4897586c2f96e709a070be683049250
dnzhelloo <dnzhelloo@icloud.com>
2023-03-27 20:40:21 +0300

    create model (user, follower)

---
+++ githubFollowersApp/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  githubFollowersApp
+//
+//  Created by deniz on 27.03.2023.
+//
+
+import Foundation
+
+struct Follower:Codable{
+    var login:String?
+    var avatarUrl:String
+}
